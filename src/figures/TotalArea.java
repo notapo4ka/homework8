7bb185f52c4c10450b9f1dde1de9479df419a2d5
notapo4ka <112635391+notapo4ka@users.noreply.github.com>
2023-03-25 22:40:09 +0200

    Update TotalArea.java

--- src/figures/TotalArea.java
+++ src/figures/TotalArea.java
@@ -2,11 +2,11 @@ package figures;
 
 public class TotalArea {
 
-    public static double totalArea(GeometryFigure figures[]) {
+    public static double totalArea(GeometryFigure[] figures) {
         double totalArea = 0.0;
 
         for (int i = 0; i < figures.length; i++) {
-            totalArea += figures[i].areaOfFigure();
+            totalArea += figures[i].getArea();
         }
         return totalArea;
     }
