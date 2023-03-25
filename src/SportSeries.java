4f9e3ee038771f196a54d0e18d75247a951ed089
notapo4ka <112635391+notapo4ka@users.noreply.github.com>
2023-03-25 22:42:20 +0200

    Update SportSeries.java

--- src/SportSeries.java
+++ src/SportSeries.java
@@ -1,33 +1,40 @@
-public class SportSeries {
+import java.util.ArrayList;
+import java.util.List;
 
-    public static String startSeries(Member members, Obstacle track, Obstacle wall) {
-        boolean isKicked = false;
-        String firstResult = "";
-        String secondResult = "";
+public class Competition {
+
+    public static String start(Participant[] participants, Obstacle[] obstacles) {
         int countOfSuccess = 0;
 
-        if (track.getLengthOrHeight() > 0) {
-            if (members.obstacleRun(track.getLengthOrHeight())) {
-                countOfSuccess++;
-                firstResult = "Учасник " + members.getName() + " пройшов перешкоду " + track.obstacleName() + " на дистанції " + track.getLengthOrHeight() + "м.";
-            } else {
-                firstResult = "Учасник " + members.getName() + " не пройшов перешкоду " + track.obstacleName() + " на дистанції " + track.getLengthOrHeight() + "м." + " Він вибуває!" + "\n" + "Пройдено: " + countOfSuccess + " перешкод/и";
-                isKicked = true;
-            }
-        } else {
-            return "All Parameters must be non-negative and greater than zero";
-        }
+        List<String> results = new ArrayList<>();
+        for (int i = 0; i < participants.length; i++) {
+            boolean isKicked = false;
+            countOfSuccess = 0;
+            for (int j = 0; j < obstacles.length - 1; j++) {
+                if (obstacles[j].getDifficulty() <= 0) {
+                    return "All Parameters must be non-negative and greater than zero";
+                }
 
-        if (wall.getLengthOrHeight() > 0) {
-            if (members.obstacleJump(wall.getLengthOrHeight()) && isKicked == false) {
-                countOfSuccess++;
-                secondResult = "Учасник " + members.getName() + " перестрибнув перешкоду " + wall.obstacleName() + " на висоті " + wall.getLengthOrHeight() + "м." + " Він перемагає!" + "\n" + "Пройдено: " + countOfSuccess + " перешкод/и";
-            } else if (isKicked == false){
-                secondResult = "Учасник " + members.getName() + " не перестрибнув перешкоду " + wall.obstacleName() + " на висоті " + wall.getLengthOrHeight() + "м." + " Він вибуває!" + "\n" + "Пройдено: " + countOfSuccess + " перешкод/и";
+                if (obstacles[j].getDifficulty() > 0) {
+                    if (participants[i].canRun(obstacles[j].getDifficulty())) {
+                        countOfSuccess++;
+                        results.add("Учасник " + participants[i].getName() + " пройшов перешкоду " + obstacles[j].getObstacleName() + " на дистанції " + obstacles[j].getDifficulty() + "м.");
+                    } else {
+                        results.add("Учасник " + participants[i].getName() + " не пройшов перешкоду " + obstacles[j].getObstacleName() + " на дистанції " + obstacles[j].getDifficulty() + "м." + " Він вибуває!" + "\n" + "Пройдено: " + countOfSuccess + " перешкод/и");
+                        isKicked = true;
+                    }
+                }
+
+                if (obstacles[j + 1].getDifficulty() > 0) {
+                    if (participants[i].canJump(obstacles[j + 1].getDifficulty()) && !isKicked) {
+                        countOfSuccess++;
+                        results.add("Учасник " + participants[i].getName() + " перестрибнув перешкоду " + obstacles[j + 1].getObstacleName() + " на висоті " + obstacles[j + 1].getDifficulty() + "м." + " Він перемагає!" + "\n" + "Пройдено: " + countOfSuccess + " перешкод/и");
+                    } else if (!isKicked) {
+                        results.add("Учасник " + participants[i].getName() + " не перестрибнув перешкоду " + obstacles[j + 1].getObstacleName() + " на висоті " + obstacles[j + 1].getDifficulty() + "м." + " Він вибуває!" + "\n" + "Пройдено: " + countOfSuccess + " перешкод/и");
+                    }
+                }
             }
-            return firstResult + "\n" + secondResult;
-        } else {
-            return "All Parameters must be non-negative and greater than zero";
         }
+        return String.join("\n", results);
     }
 }
