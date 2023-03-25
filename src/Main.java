3d6179996ff18ba5384a27c45133ba4620d9b2dd
notapo4ka <112635391+notapo4ka@users.noreply.github.com>
2023-03-25 22:39:08 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,21 @@ import figures.*;
 public class Main {
 
     public static void main(String[] args) {
-        GeometryFigure figures[] = new GeometryFigure[3];
+        GeometryFigure[] figures = new GeometryFigure[3];
         figures[0] = new Circle(5);
         figures[1] = new Square(2);
         figures[2] = new Triangle(5, 4);
-        Member members[] = new Member[3];
-        members[0] = new Human("Sam");
-        members[1] = new Cat("Huggy");
-        members[2] = new Robot("B-El8");
-        Obstacle obstacles[] = new Obstacle[2];
+        Participant[] participants = new Participant[3];
+        participants[0] = new Human("Sam");
+        participants[1] = new Cat("Huggy");
+        participants[2] = new Robot("B-El8");
+        Obstacle[] obstacles = new Obstacle[2];
         obstacles[0] = new JoggingTrack(15);
         obstacles[1] = new Wall(4);
 
-        System.out.println("Площа кола: " + figures[0].areaOfFigure());
-        System.out.println("Площа квадрата: " + figures[1].areaOfFigure());
-        System.out.println("Площа трикутника: " + figures[2].areaOfFigure());
+        System.out.println("Площа кола: " + figures[0].getArea());
+        System.out.println("Площа квадрата: " + figures[1].getArea());
+        System.out.println("Площа трикутника: " + figures[2].getArea());
 
         System.out.println();
 
@@ -25,31 +25,27 @@ public class Main {
 
         System.out.println();
 
-        System.out.println("Ім'я людини: " + members[0].getName());
-        System.out.println("Ім'я кота: " + members[1].getName());
-        System.out.println("Назва робота: " + members[2].getName());
+        System.out.println("Ім'я людини: " + participants[0].getName());
+        System.out.println("Ім'я кота: " + participants[1].getName());
+        System.out.println("Назва робота: " + participants[2].getName());
 
         System.out.println();
 
-        System.out.println(members[0].run() + "\n" + members[0].jump());
-        System.out.println(members[1].run() + "\n" + members[1].jump());
-        System.out.println(members[2].run() + "\n" + members[2].jump());
+        System.out.println(participants[0].run() + "\n" + participants[0].jump());
+        System.out.println(participants[1].run() + "\n" + participants[1].jump());
+        System.out.println(participants[2].run() + "\n" + participants[2].jump());
 
         System.out.println();
 
-        System.out.println(obstacles[0].overcome(members[0]));
-        System.out.println(obstacles[1].overcome(members[0]));
-        System.out.println(obstacles[0].overcome(members[1]));
-        System.out.println(obstacles[1].overcome(members[1]));
-        System.out.println(obstacles[0].overcome(members[2]));
-        System.out.println(obstacles[1].overcome(members[2]));
+        System.out.println(obstacles[0].overcome(participants[0]));
+        System.out.println(obstacles[1].overcome(participants[0]));
+        System.out.println(obstacles[0].overcome(participants[1]));
+        System.out.println(obstacles[1].overcome(participants[1]));
+        System.out.println(obstacles[0].overcome(participants[2]));
+        System.out.println(obstacles[1].overcome(participants[2]));
 
         System.out.println();
 
-        System.out.println(SportSeries.startSeries(members[0], obstacles[0], obstacles[1]));
-        System.out.println();
-        System.out.println(SportSeries.startSeries(members[1], obstacles[0], obstacles[1]));
-        System.out.println();
-        System.out.println(SportSeries.startSeries(members[2], obstacles[0], obstacles[1]));
+        System.out.println(Competition.start(participants, obstacles));
     }
 }
